dbc2fba150b9e58ce11ee1569c5e5f018e8642ec
Kuldeep-805 <kuldeepsingh0650@gmail.com>
2025-11-25 22:43:10 +0530

    Enhance azurerm_storage_account with CMK support
    Added support for customer-managed keys (CMK) and system-assigned identity to the Azure Storage Account resource.

--- Modules/azurerm_storage_account/main.tf
+++ Modules/azurerm_storage_account/main.tf
@@ -1,9 +1,29 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "var.sa_name"
+  name                     = var.sa_name
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
-  tags = var.tags
+  # Required for CMK
+  identity {
+    type = "SystemAssigned"
   }
+
+  # CMK Encryption (THIS FIXES CKV2_AZURE_1)
+  encryption {
+    key_source = "Microsoft.Keyvault"
+    key_vault_key_id = azurerm_key_vault_key.cmk.id
+
+    services {
+      blob {
+        enabled = true
+      }
+      file {
+        enabled = true
+      }
+    }
+  }
+
+  tags = var.tags
+}
