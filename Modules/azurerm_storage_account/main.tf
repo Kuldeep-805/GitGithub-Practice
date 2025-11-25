59d577946792d1a03ed7a39cfe9c8f8e11a95311
Kuldeep-805 <kuldeepsingh0650@gmail.com>
2025-11-25 22:33:35 +0530

    Update main.tf

--- Modules/azurerm_storage_account/main.tf
+++ Modules/azurerm_storage_account/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "var.sa_name"
+  name                     = var.sa_name
   resource_group_name      = var.rg_name
   location                 = var.location
   account_tier             = "Standard"
